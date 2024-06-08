3355bb06eb8f947ca72f52ed60023933fbaf77bc
Lucas Lora <lucasfelippilora@gmail.com>
2024-06-07 22:24:30 -0300

    Criação dos arquivos inicias das interfaces e presentes dos registro de gastos

---
+++ CarbonTracker/Views/Interfaces/IRegistroGastosEletrodomesticoView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarbonTracker.Views.Interfaces
+{
+    public interface IRegistroGastosEletrodomesticoView
+    {
+
+        #region Propriedades
+
+        DateTime Dia { get; set; }
+        string SearchValue { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        #endregion
+
+        #region Eventos
+
+        event EventHandler RegistrarDia;
+        event EventHandler TrocarDia;
+        event EventHandler SearchEvent;
+        event EventHandler SalvarEvent;
+        event EventHandler RestaurarEvent;
+
+        #endregion
+
+        #region Métodos
+
+        void BloquearData(bool bloquear);
+        void SetGastosEletrodomesticoListBindingSource(BindingSource gastosEletrodomesticoList);
+        void Show();
+
+        #endregion
+
+    }
+}
---
+++ CarbonTracker/Views/Interfaces/IRegistroGastosTransporteView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarbonTracker.Views.Interfaces
+{
+    public interface IRegistroGastosTransporteView
+    {
+
+        #region Propriedades
+
+        DateTime Dia { get; set; }
+        string SearchValue { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        #endregion
+
+        #region Eventos
+
+        event EventHandler RegistrarDia;
+        event EventHandler TrocarDia;
+        event EventHandler SearchEvent;
+        event EventHandler SalvarEvent;
+        event EventHandler RestaurarEvent;
+
+        #endregion
+
+        #region Métodos
+
+        void BloquearData(bool bloquear);
+        void SetGastosTransporteListBindingSource(BindingSource gastosTransporteList);
+        void Show();
+
+        #endregion
+
+    }
+}
